17fdfc88abdaf955f99f7ccfee45cb935c8ca9f3
Martin <martin_lenart@icloud.com>
2023-04-14 19:03:18 +0200

    update

--- docker/create_sqledge_docker.sh
+++ docker/create_sqledge_docker.sh
@@ -4,4 +4,4 @@
 docker pull mcr.microsoft.com/azure-sql-edge
 
 # create and install a sql edge docker 
-docker run --cap-add SYS_PTRACE -e 'ACCEPT_EULA=1' -e 'MSSQL_SA_PASSWORD=skYhgS@83#aQ' -p 1433:1433 --name sqledgecontainer -d mcr.microsoft.com/azure-sql-edge
+docker run --cap-add SYS_PTRACE -e 'ACCEPT_EULA=1' -e 'MSSQL_SA_PASSWORD=skYhgS@83#aQ' -p 1433:1434 --name sqledgecontainer1 -d mcr.microsoft.com/azure-sql-edge
